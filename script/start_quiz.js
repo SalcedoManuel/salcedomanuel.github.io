28b71097fe8eb3fc4a00b510ec57d9fa3d7ab5d4
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2023-02-12 18:03:26 +0100

    audio

--- script/start_quiz.js
+++ script/start_quiz.js
@@ -1,5 +1,5 @@
 
-function start_audio_quiz() {
+function start_quiz() {
     document.getElementById("wrapper0").style.display = "none";
     document.getElementById("wrapper1").style.display = "flex";
     console.info("Dentro...")
